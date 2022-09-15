38e497851a660cf4f96b96dfbaf7b0f256d0ba80
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2022-09-15 13:08:33 -0600

    fix script to run go tests

--- gotest.sh
+++ gotest.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
 
-set -u
+set -euo pipefail
 
-BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE_ID="293c201b-3693-7f52-a8da-44ea8a0ca2cd"
-BUILDKITE_PLUGIN_VAULT_SECRETS_SECRET_ID="$(vault write -field=secret_id -force auth/approle/role/buildkite-agent/secret-id)"
+gotestsum --junitfile junit.xml .
 
-echo "$BUILDKITE_PLUGIN_VAULT_SECRETS_SECRET_ID"
+curl \
+    -X POST \
+    --fail-with-body \
+    -H "Authorization: Token token=\"$BUILDKITE_ANALYTICS_TOKEN\" " \
+    -F "data=@junit.xml" \
+    -F "format=junit" \
+    -F "run_env[CI]=buildkite" \
+    -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+    -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+    -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+    -F "run_env[branch]=$BUILDKITE_BRANCH" \
+    -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+    -F "run_env[message]=$BUILDKITE_MESSAGE" \
+    -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+    https://analytics-api.buildkite.com/v1/uploads
 
-token=$(vault write -field=token auth/approle/login role_id="$BUILDKITE_PLUGIN_VAULT_SECRETS_ROLE_ID" \
-     secret_id="$BUILDKITE_PLUGIN_VAULT_SECRETS_SECRET_ID")
-
-echo $token
-
-# gotestsum --junitfile junit.xml .
-
-# curl \
-#     -X POST \
-#     --fail-with-body \
-#     -H "Authorization: Token token=\"$BUILDKITE_ANALYTICS_TOKEN\" " \
-#     -F "data=@junit.xml" \
-#     -F "format=junit" \
-#     -F "run_env[CI]=buildkite" \
-#     -F "run_env[key]=$BUILDKITE_BUILD_ID" \
-#     -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
-#     -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
-#     -F "run_env[branch]=$BUILDKITE_BRANCH" \
-#     -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
-#     -F "run_env[message]=$BUILDKITE_MESSAGE" \
-#     -F "run_env[url]=$BUILDKITE_BUILD_URL" \
-#     https://analytics-api.buildkite.com/v1/uploads
-
-# rm junit.xml
+rm junit.xml
